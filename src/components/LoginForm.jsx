4416ac20200d93d1c7e120256fb2130f6961b0d4
Luca Bonini <luca.bonini93@gmail.com>
2020-09-15 14:34:53 +0200

    add auth error message

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
-import { Button, Container, Form } from 'semantic-ui-react'
+import { Button, Container, Form, Message } from 'semantic-ui-react'
 import axios from 'axios'
 import { serverAuthPath, serverUrl } from '../../config.js'
 import { useDispatch } from "react-redux";
 import io from 'socket.io-client'
 import { getAuth } from '../helpers'
-import { AUTH_USER } from '..reducers/'
+import { AUTH_USER } from '../reducers'
 
 function Login() {
   let [username, setUsername] = useState(null)
   let [password, setPassword] = useState(null)
+  let [error, setError] = useState(null)
   const dispatch = useDispatch()
   const handleSubmit = async () => {
     try {
@@ -24,13 +25,25 @@ function Login() {
           socket: io(serverUrl, getAuth())
         }
       })
-    } catch (error) {
-      //   // TODO HANDLE WRONG CREDENTIALS
-      console.log(error)
+    } catch (err) {
+      setError(err)
     }
   }
+
+  const renderError = () => {
+    if (error) {
+      return (
+        <Message color="red">
+          <Message.Header>Authorization failed</Message.Header>
+          <p>Credentials are not valid, check your username and password</p>
+        </Message>
+      )
+    }
+  }
+
   return (
     <Container>
+      {renderError()}
       <Form onSubmit={() => handleSubmit()}>
         <Form.Field value={username} onChange={(e) => setUsername(e.target.value)}>
           <label>Username</label>
